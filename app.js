1d7a3234a15d7cd9b53e6d0d4fc4e4872d7a19d0
MaricruzJ <maricruzj.er@gmail.com>
2020-03-26 22:25:53 -0500

    add new styles

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
+document.addEventListener('DOMContentLoaded', () => {
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems, {});
 });
-
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-var collapsibleElem = document.querySelector('.collapsible');
-var collapsibleInstance = M.Collapsible.init(collapsibleElem, {});
